bb12ec42be48b006cc8a044686e1d273d89e6584
Vinayak Iyer <sahacharyapvtltd@gmail.com>
2024-07-05 12:33:02 +0530

    added consolelogs for debugging scraper info

--- api/services/scrapperApi/fetchScrapperApi.js
+++ api/services/scrapperApi/fetchScrapperApi.js
@@ -5,15 +5,16 @@ export const getWebsiteMetadata = async(url) => {
   console.log('url>>>>>> ',url)
     try {
       const { data: htmlData = '' } = await axios.get(url) || {}
-      console.log('htmlData RECVD from AXIOS>> ',htmlData)
       if(!htmlData){
         throw new Error('Invalid url!')
       }
+      console.log('HTML DATA FOUND')
       return {
         status: true,
         data: travHtmlAndGetData(htmlData)
       }
     } catch (error) {
+      console.log('ERROR IN getWebsiteMetadata')
       return {
         status: false,
         message: (error.message || 'Invalid Url!')
