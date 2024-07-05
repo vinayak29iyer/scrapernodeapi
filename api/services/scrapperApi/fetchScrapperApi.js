f1ae61e0060d8ffcdbd0a2ceb1b52dee7493a90c
Vinayak Iyer <sahacharyapvtltd@gmail.com>
2024-07-05 14:50:33 +0530

    timeout and headers added in axios and removed packagelock from gitignore

--- api/services/scrapperApi/fetchScrapperApi.js
+++ api/services/scrapperApi/fetchScrapperApi.js
@@ -4,7 +4,12 @@ import {travHtmlAndGetData} from './fetchScrapperApiHelper'
 export const getWebsiteMetadata = async(url) => {
   console.log('url>>>>>> ',url)
     try {
-      const { data: htmlData = '' } = await axios.get(url) || {}
+      const { data: htmlData = '' } = await axios.get(url,{
+        timeout: 40000, // Timeout in milliseconds (5 seconds)
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0'
+        }
+      }) || {}
       if(!htmlData){
         throw new Error('Invalid url!')
       }
