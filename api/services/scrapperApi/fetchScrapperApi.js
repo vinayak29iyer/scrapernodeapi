7adfa575e37652a8bf358ebe0444408aa1ccfcf2
Vinayak Iyer <sahacharyapvtltd@gmail.com>
2024-07-05 13:35:12 +0530

    uninstalled axios used puppeteer for accurate results

--- api/services/scrapperApi/fetchScrapperApi.js
+++ api/services/scrapperApi/fetchScrapperApi.js
@@ -1,16 +1,25 @@
-import axios from 'axios'
+// import axios from 'axios'
+import puppeteer from 'puppeteer'
 import {travHtmlAndGetData} from './fetchScrapperApiHelper'
 
 export const getWebsiteMetadata = async(url) => {
   console.log('url>>>>>> ',url)
     try {
-      const { data: htmlData = '' } = await axios.get(url) || {}
+      const htmlData = async() => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(url);
+        const html = await page.content(); 
+        await browser.close();
+        return html
+    }
+     /*  const { data: htmlData = '' } = await axios.get(url) || {}
       if(!htmlData){
         throw new Error('Invalid url!')
-      }
+      } */
       return {
         status: true,
-        data: travHtmlAndGetData(htmlData)
+        data: travHtmlAndGetData(await htmlData())
       }
     } catch (error) {
       console.log('ERROR IN getWebsiteMetadata')
